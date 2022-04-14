58e71b23f52633040877cc23fcd5fa3e690ef5b5
jvp <joelvpayne@pm.me>
2022-04-14 09:41:48 -0700

    Added variables for splitting input into url elements

--- canlii-pdfetch.py
+++ canlii-pdfetch.py
@@ -3,22 +3,35 @@ import PyPDF2
 import re
 
 # Ask user for citation to fetch
-citation = input("Enter the citation of the case to fetch: \n")
+requested_cite = input("Enter the citation of the case to fetch: \n")
+cite_split = requested_cite.split()
+print(cite_split)
+
+year = cite_split[0]
+court = cite_split[1]
+juris = court[0:2]
+case_no = cite_split[2]
+citation = ''.join(cite_split)
+
+print(requested_cite)
+print(juris)
+print(citation)
 
 # Construct the url from user input and download
-url = 'https://www.canlii.org/en/bc/bcsc/doc/2021/' + citation + '/' + citation + '.pdf'
+url = 'https://www.canlii.org/en/' + juris + '/' + court + '/doc/2021/' + citation + '/' + citation + '.pdf'
 # CanLII url format: https://www.canlii.org/en/bc/bcsc/doc/2021/2021bcsc32/2021bcsc32.pdf
 r = requests.get(url, stream=True)
 
-# Rename and save pdf
-#temp_filename = 'temp_' + citation + '.pdf'
-temp_filename = citation + '.pdf'
+print(url)
 
-with open(temp_filename, 'wb') as f:
+# Rename and save pdf
+filename = citation + '.pdf'
+
+with open(filename, 'wb') as f:
     f.write(r.content)
         
 # Create pdf file object
-#pdf_file_object = open(temp_filename, 'rb')
+#pdf_file_object = open(filename, 'rb')
     
 # Create pdf reader object
 #pdf_reader_object = PyPDF2.PdfFileReader(pdf_file_object)
@@ -36,8 +49,6 @@ with open(temp_filename, 'wb') as f:
 #s = first_page_text
 #party_name_regex = re.search(r'Citation:( [a-zA-Z]+ )v.', s).group(1)
 #party_name_regex = re.search(r'Citation:\s+(.+\n)', s).group(1)
-
-
 
 #print(party_name_regex)
 
